4948b26d7175156824b6f961ce64a7e9933fbda3
Yahya <idlahcencih@gmail.com>
2025-04-20 14:34:17 +0100

    Fix Hero component path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./styles/app.scss";
 import Header from "./components/header";
-import Hero from "./components/Hero";
+import Hero from "./components/Hero.jsx";
 import Section1 from "./components/section1";
 import Section2 from "./components/section2";
 import Section3 from "./components/section3";
